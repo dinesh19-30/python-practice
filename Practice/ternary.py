3e4a84aa094d04c37f6e1f929efdf04a682fb40d
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-04-01 20:32:14 +0530

    ternary opr

---
+++ Practice/ternary.py
@@ -0,0 +1,25 @@
+#Ternatory operator - It is defined as the if else statement in single line.
+
+#1
+num = int(input())
+result = "pos" if num >= 0 else "neg"
+print(result)
+
+#2
+result1 = "Even" if num%2==0 else "odd"
+print(result1)
+
+#3
+a=5
+b=2
+max= a if a>b else b
+print(f"no:{max}")
+
+#4
+a1=2
+a2=6
+min= a if a<b else b
+print(f"no:{min}")
+
+
+
