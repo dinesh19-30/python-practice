2bccd28db548fbd4462ac4897efc4be9290e4254
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-04-01 10:36:10 +0530

    solved two problems in leet code

--- Leetcode/single number.py
+++ Leetcode/single number.py
@@ -2,11 +2,10 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
+        res=0
         for n in nums:
-            res = res^n
-        return res
-
+            res=res^n
+        return res    
 if __name__ == "__main__":
     solution = Solution()
     nums = [4, 1, 2, 1, 2]
