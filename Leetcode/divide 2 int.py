c05baaaa874a2ecc8fc99887724199ac3cf41bca
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-04-08 20:50:01 +0530

    2D collections

--- Leetcode/divide 2 int.py
+++ Leetcode/divide 2 int.py
@@ -1,12 +1,16 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
-            sig = -1
-        else:
-            sig = 1
+        INT_MAX=2**31-1
+        INT_MIN= -2**31
+
+        if dividend==INT_MIN and divisor==-1:
+            return INT_MAX
+        
+        negative= (dividend<0) ^ (divisor<0)
+
         dividend = abs(dividend)
         divisor = abs(divisor)
-        res = 0
+        quotient = 0
 
         while dividend >= divisor:
             b = divisor
