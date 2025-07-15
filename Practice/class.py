2fe434a715b0dbde607a03ab22f5b89872eb7c08
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-07-15 09:38:13 +0530

    class 2

--- Practice/class.py
+++ Practice/class.py
@@ -2,11 +2,25 @@ class laptop:
     def __init__(self):
         self.price=1000
         self.ram=""
-        self.processor=""   
+        self.processor=""  
+    def display(self):
+        print("ram =",self.ram) 
+        print("processor =",self.processor)    
+        print(self.price)
 hp=laptop()
+dell=laptop()
+mac=laptop()
 hp.price=f"Maximum price :{10000}"
 hp.processor="i7"
 hp.ram="5gb"
-print(hp.price)
-print(hp.processor)
-print(hp.ram)
+
+dell.price=f"Maximum price :{20000}"
+dell.processor="i7"
+dell.ram="5gb"
+
+mac.price=f"Maximum price :{300000}"
+mac.processor="i7"
+mac.ram="5gb"
+hp.display()
+dell.display()
+mac.display()
